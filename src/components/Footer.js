9159b79effe724eded0c5c3325326197d3d948cb
monsterkosh <mnnnkosh@gmail.com>
2021-05-05 00:11:26 -0300

    full commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Created by Emiliano Kosh</p>
+    </div>
+  );
+};
+
+export default Footer;
